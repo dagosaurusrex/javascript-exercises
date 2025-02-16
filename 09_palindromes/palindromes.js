fc34e9f1caeaf8eda6977811d77be5fe9800019c
Sarah D'Agostino <166957570+dagosaurusrex@users.noreply.github.com>
2025-02-15 16:44:37 -0800

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let arr = word.split('');
 
+    let charSet=['!',',',' ','.'];
+    function allowedChar(char) {
+        if (!charSet.includes(char))
+        return true;
+        else return false;
+    }
+
+    arr = arr.filter(allowedChar)
+
+
+
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] !== arr[arr.length-i-1])
+        return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
