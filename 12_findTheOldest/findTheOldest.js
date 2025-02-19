cf0bde06b5071fb6e49e8555c42bcea4ac8c9b31
Sarah D'Agostino <166957570+dagosaurusrex@users.noreply.github.com>
2025-02-18 18:37:07 -0800

    "find the oldest" exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    people.forEach((person, index) => {
+        if (!person.yearOfDeath)
+        person.age = currentYear - person.yearOfBirth;
+        else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    people.sort((a,b) => {
+        if (a.age > b.age) return -1;
+        if (a.age == b.age) return 0;
+        if (a.age < b.age) return 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
